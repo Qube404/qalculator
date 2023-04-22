d0802618c5732ff8b11569a0c7bbb725af0f586f
Qube404 <qube404@protonmail.com>
2023-04-23 08:13:19 +1000

    Added css provider

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod qalculator_window;
 
+use gtk::gdk::Display;
 use qalculator_window::QalculatorWindow;
 
-use gtk::prelude::*;
+use gtk::{prelude::*, CssProvider, style_context_add_provider_for_display};
 use gtk::{gio, glib, Application};
 
 const APP_ID: &str = "com.github.Qube404.Qalculator";
@@ -15,9 +16,21 @@ fn main() -> glib::ExitCode {
         .application_id(APP_ID)
         .build();
 
+    app.connect_startup(|_| load_css());
     app.connect_activate(activate);
 
     app.run()
+}
+
+fn load_css() {
+    let provider = CssProvider::new();
+    provider.load_from_path("resources/styles.css");
+
+    style_context_add_provider_for_display(
+        &Display::default().expect("Could not connect to a display."), 
+        &provider, 
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION
+    );
 }
 
 fn activate(app: &Application) {
